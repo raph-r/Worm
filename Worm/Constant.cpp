febb3bb8abd48fd6c2d41f814136b3e2936b9e44
rrtf <raphaelrtf@gmail.com>
2019-08-10 15:09:13 -0300

    Create class of constant values

---
+++ Worm/Constant.cpp
@@ -0,0 +1,8 @@
+#include "Constant.h"
+
+const unsigned int Constant::POWERUP_WIDTH = 10;
+const unsigned int Constant::POWERUP_HEIGHT = 10;
+const unsigned int Constant::SCREEN_WIDTH = 640;
+const unsigned int Constant::SCREEN_HEIGHT = 480;
+const unsigned int Constant::HALF_SCREEN_WIDTH = Constant::SCREEN_WIDTH / 2;
+const unsigned int Constant::HALF_SCREEN_HEIGHT = Constant::SCREEN_HEIGHT / 2;
---
+++ Worm/Constant.h
@@ -0,0 +1,12 @@
+#pragma once
+class Constant
+{
+	public:
+		static const unsigned int POWERUP_WIDTH;
+		static const unsigned int POWERUP_HEIGHT;
+		static const unsigned int SCREEN_WIDTH;
+		static const unsigned int SCREEN_HEIGHT;
+		static const unsigned int HALF_SCREEN_WIDTH;
+		static const unsigned int HALF_SCREEN_HEIGHT;
+};
+
