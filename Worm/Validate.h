15f995cd5536afc56a955e636d6b125a3d0ad735
rrtf <raphaelrtf@gmail.com>
2019-08-10 17:14:57 -0300

    Ádd comment to namespace Validate

--- Worm/Validate.h
+++ Worm/Validate.h
@@ -2,7 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * <EN>
+ * Namespace of validation methods
+ * <PT-Br>
+ * Namespace de metodos de validacao
+*/
 namespace Validate
 {
+	/**
+	 * <EN>
+	 * If the received value is false, print following message:
+	 *  'object_name' wasn`t initialized.	
+	 * Then, close the game
+	 * @param[in] result_to_validate - Value to validate
+	 * @param[in] object_name - Object name
+	 *
+	 * <PT-Br>
+	 * Caso o valor recebido for false, imprime a mensagem:
+	 *  'object_name' wasn`t initialized.
+	 * Entao fecha o jogo
+	 * @param[in] result_to_validate - valor para validar
+	 * @param[in] object_name - Nome do objeto
+	*/
 	void object_was_initialized(const bool& result_to_validate, const char* object_name);
 }
